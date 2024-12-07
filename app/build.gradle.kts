a32a13351eed57f5769a6067f5c26f8b41b65a75
mh4647 <mh4647@zebra.com>
2024-12-07 21:47:06 +0200

    Remove release singin config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,7 +64,6 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            signingConfig = signingConfigs.getByName("release")
         }
     }
 
